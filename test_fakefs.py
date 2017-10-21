23719841579c0cecae90f427f306332216eb98a0
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-10-21 20:03:36 +0200

    first test

---
+++ test_fakefs.py
@@ -0,0 +1,15 @@
+import unittest
+import fakefs
+from nose.tools import assert_equal
+
+class FakeTestCase(unittest.TestCase):
+    def run(self, result=None):
+        self.fs = fakefs.FakeFilesystem()
+        with self.fs.monkey.patch():
+            super(FakeTestCase, self).run(result)
+
+    def test_open_write(self):
+        with open('/a.txt', 'w') as f:
+            f.write('abc')
+        assert_equal("abc", self.fs.content_for('/a.txt'))
+    
