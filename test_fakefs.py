6bbf750f67a4e518f9218d3f2d265576deef9834
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-10-22 18:01:06 +0200

    test `copy`

--- test_fakefs.py
+++ test_fakefs.py
@@ -3,6 +3,8 @@ import fakefs
 from nose.tools import assert_equal, assert_true, assert_false, raises
 
 import os
+import shutil
+
 
 class FakeTestCase(unittest.TestCase):
     def run(self, result=None):
@@ -76,3 +78,27 @@ class FakeTestCase(unittest.TestCase):
         self.fs.add_file('/file', '123')
         stat = os.stat('/file')
         assert_equal(stat.st_mtime, 0)
+
+    @raises(FileNotFoundError)
+    def test_remove_missing(self):
+        os.remove('/nope')
+    
+    def test_remove(self):
+        self.fs.add_file('/file', '123')
+        os.remove('/file')
+        assert_false(os.path.isfile('/file'))
+
+    # shutil
+    @raises(OSError)
+    def test_copy_missing(self):
+        shutil.copy('/nope', 'whatever')
+
+    def test_copy_old_still_there(self):
+        self.fs.add_file('/a', 'a')
+        shutil.copy('/a', '/b')
+        assert_true(os.path.isfile('/a'))
+
+    def test_copy_new_content(self):
+        self.fs.add_file('/a', 'a')
+        shutil.copy('/a', '/b')
+        assert_equal(self.fs.content_for('/b'), b'a')
